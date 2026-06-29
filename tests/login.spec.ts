fafc41b623288124570b73cf224d7f93458f26f4
Εκλεκτός εν Χριστώ <eklektos@Dungeon.local>
2026-06-29 16:45:20 +0530

    login success

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,21 @@
 import { test, expect } from '@playwright/test';
 
+const sauceDemo = 'https://www.saucedemo.com/'
+
 test('has title', async ({ page }) => {
-    await page.goto('https://saucedemo.com/');
+    await page.goto(sauceDemo);
 
     await expect(page).toHaveTitle('Swag Labs')
+})
+
+test('login success', async ({ page }) => {
+    await page.goto(sauceDemo);
+
+    await page.getByPlaceholder('Username').fill('standard_user')
+
+    await page.getByPlaceholder('Password').fill('secret_sauce')
+
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page).toHaveURL(`${sauceDemo}inventory.html`)
 })
