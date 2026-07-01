09ce91a4e369136a22e6f289acd9984c7f1861af
Εκλεκτός εν Χριστώ <eklektos@Dungeon.local>
2026-07-01 15:13:33 +0530

    refactor login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,39 +1,25 @@
 import { test, expect } from '@playwright/test';
+import * as auth from './pages/LoginPOM'
 
 const baseURL = 'https://www.saucedemo.com/'
 
 test('has title', async ({ page }) => {
-    await page.goto(baseURL)
-
-    await expect(page).toHaveTitle('Swag Labs')
+    await auth.goto(page)
 })
 
 test('login success', async ({ page }) => {
-    await page.goto(baseURL)
-    await page.getByPlaceholder('Username').fill('standard_user')
-    await page.getByPlaceholder('Password').fill('secret_sauce')
-    await page.getByRole('button', { name: 'Login' }).click()
-
-    await expect(page).toHaveURL(`${baseURL}inventory.html`)
+    await auth.login(page, 'standard_user', 'secret_sauce')
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
 })
 
 test('login failed', async ({ page }) => {
-    await page.goto(baseURL)
-    await page.getByPlaceholder('Username').fill('standard_user')
-    await page.getByPlaceholder('Password').fill('wrong.kd-pass')
-    await page.getByRole('button', { name: 'Login' }).click()
-
-    // await expect(page).not.toHaveURL(`${sauceDemo}inventory.html`)
-
+    await auth.login(page, 'standard_user', 'wrongpasswd')
     await expect(page.getByRole('heading',
         { name: 'Username and password do not match' })).toBeVisible()
 })
 
 test('locked out user', async ({ page }) => {
-    await page.goto(baseURL)
-    await page.getByPlaceholder('Username').fill('locked_out_user')
-    await page.getByPlaceholder('Password').fill('secret_sauce')
-    await page.getByRole('button', { name: 'Login' }).click()
+    await auth.login(page, 'locked_out_user', 'secret_sauce')
 
     // console.log(await page.getByRole('heading',
     //     { name: 'Sorry, this user has been locked out' }).textContent())
@@ -43,8 +29,7 @@ test('locked out user', async ({ page }) => {
 })
 
 test('empty fields login', async ({ page }) => {
-    await page.goto(baseURL)
-    await page.getByRole('button', { name: 'Login' }).click()
+    await auth.login(page, '', '')
 
     // console.log(await page.getByRole('heading',
     //     { name: 'Username is required' }).textContent())
@@ -54,9 +39,7 @@ test('empty fields login', async ({ page }) => {
 })
 
 test('empty password field', async ({ page }) => {
-    await page.goto(baseURL)
-    await page.getByPlaceholder('Username').fill('standard_user')
-    await page.getByRole('button', { name: 'Login' }).click()
+    await auth.login(page, 'standard_user', '')
 
     // console.log(await page.getByRole('heading',
     //     { name: 'Password is required' }).textContent())
---
+++ tests/pages/LoginPOM.ts
@@ -0,0 +1,14 @@
+import { expect, type Page } from "@playwright/test";
+
+export async function goto(page: Page) {
+    await page.goto('https://www.saucedemo.com/')
+    await expect(page).toHaveTitle('Swag Labs')
+    return page.url()
+}
+
+export async function login(page: Page, user: string, pass: string) {
+    await goto(page)
+    await page.getByPlaceholder('Username').fill(user)
+    await page.getByPlaceholder('Password').fill(pass)
+    await page.getByRole('button', { name: 'Login' }).click()
+}
