287cba82f060c94ba9082c012af7a01ea5aa84c0
Εκλεκτός εν Χριστώ <eklektos@Dungeon.local>
2026-07-04 10:35:31 +0530

    update

--- tests/pages/CartPOM.ts
+++ tests/pages/CartPOM.ts
@@ -1,42 +1,54 @@
 import { type Page } from "@playwright/test";
 
 export async function addItem(page: Page) {
-    await page.getByRole('button', { name: 'Add to cart' }).nth(1).click()
-    const cartBadge = page.getByTestId('shopping-cart-badge')
-    return cartBadge
+    await page.getByRole("button", { name: "Add to cart" }).nth(1).click();
+    const cartBadge = page.getByTestId("shopping-cart-badge");
+    return cartBadge;
 }
 
 export async function removeItem(page: Page) {
-    await page.getByTestId('shopping-cart-badge').click()
-    await page.getByRole('button', { name: 'Remove' }).click()
-    return page.getByTestId('shopping-cart-badge')
+    await page.getByTestId("shopping-cart-badge").click();
+    await page.getByRole("button", { name: "Remove" }).click();
+    return page.getByTestId("shopping-cart-badge");
 }
 
 export async function itemInfoVisible(page: Page) {
-    await page.getByTestId('inventory-item-name').nth(2).click()
+    await page.getByTestId("inventory-item-name").nth(2).click();
     return {
-        itemTitle: page.getByTestId('inventory-item-name'),
-        itemPrice: page.getByTestId('inventory-item-price'),
-        itemDesc: page.getByTestId('inventory-item-desc')
-    }
+        itemTitle: page.getByTestId("inventory-item-name"),
+        itemPrice: page.getByTestId("inventory-item-price"),
+        itemDesc: page.getByTestId("inventory-item-desc"),
+    };
 }
 
-export async function getFillInfo(page: Page, firstName: string, lastName: string, postalCode: string) {
-    await page.getByTestId('firstName').fill(firstName)
-    await page.getByTestId('lastName').fill(lastName)
-    await page.getByTestId('postalCode').fill(postalCode)
+export async function getFillInfo(
+    page: Page,
+    firstName: string,
+    lastName: string,
+    postalCode: string,
+) {
+    await page.getByTestId("firstName").fill(firstName);
+    await page.getByTestId("lastName").fill(lastName);
+    await page.getByTestId("postalCode").fill(postalCode);
 }
 
-export async function cartCheckout(page: Page, firstName: string, lastName: string, postalCode: string) {
-    await page.getByTestId('shopping-cart-badge').click()
-    await page.getByRole('button', { name: 'checkout' }).click()
-    await getFillInfo(page, firstName, lastName, postalCode)
-    await page.getByRole('button', { name: 'continue' }).click()
-    await page.getByText('All Rights Reserved.').scrollIntoViewIfNeeded()
-    await page.getByRole('button', { name: 'Finish' }).click()
+export async function cartCheckout(
+    page: Page,
+    firstName: string,
+    lastName: string,
+    postalCode: string,
+) {
+    await page.getByTestId("shopping-cart-badge").click();
+    await page.getByRole("button", { name: "checkout" }).click();
+    await getFillInfo(page, firstName, lastName, postalCode);
+    await page.getByRole("button", { name: "continue" }).click();
+    await page.getByText("All Rights Reserved.").scrollIntoViewIfNeeded();
+    await page.getByRole("button", { name: "Finish" }).click();
 
     return {
         pageUrl: page.url(),
-        thankYou: await page.getByText('Thank you for your order!', { exact: true })
-    }
+        thankYou: await page.getByText("Thank you for your order!", {
+            exact: true,
+        }),
+    };
 }
